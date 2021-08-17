3eeacdce64f50d7669463538b0eb6a193708da26
ukyi <devcat.js@gmail.com>
2021-08-17 15:11:34 +0900

    feat: env util 추가

---
+++ src/utils/env/index.ts
@@ -0,0 +1,5 @@
+import { ConfigService } from '@nestjs/config';
+import { EnvVariables } from '@/types/env';
+
+const configService = new ConfigService<EnvVariables>();
+export const getEnv = <K extends keyof EnvVariables>(key: K): EnvVariables[K] => <EnvVariables[K]>configService.get(key);
