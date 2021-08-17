fa63bbeb68237b4bd70dded4ec503f025c9eb0f6
ukyi <devcat.js@gmail.com>
2021-08-17 15:54:56 +0900

    feat: getMetadataArgsStorage 추가

--- src/config/typeorm/index.ts
+++ src/config/typeorm/index.ts
@@ -1,28 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
-import { join } from 'path';
+import { getMetadataArgsStorage } from 'typeorm';
 import { getEnv } from '@/utils';
-import { User } from '@/user/entities/user.entity';
 
 @Injectable()
 export class TypeormService implements TypeOrmOptionsFactory {
-  getEntities(): any[] {
-    const isNetlifyBuild = getEnv('NETLIFY_BUILD') === 'true';
-    if (!isNetlifyBuild) return [join(process.cwd(), 'dist/src', '**/**.entity.{t,j}s')];
-
-    return [User];
-  }
-
-  async createTypeOrmOptions(): Promise<TypeOrmModuleOptions> {
-    return {
-      type: 'mongodb',
-      url: getEnv('DATABASE_URL'),
-      entities: this.getEntities(),
-      synchronize: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      logging: true,
-      keepConnectionAlive: true,
-    };
-  }
+  createTypeOrmOptions = async (): Promise<TypeOrmModuleOptions> => ({
+    type: 'mongodb',
+    url: getEnv('DATABASE_URL'),
+    entities: getMetadataArgsStorage().tables.map(tbl => tbl.target),
+    synchronize: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    logging: true,
+    keepConnectionAlive: true,
+  });
 }
