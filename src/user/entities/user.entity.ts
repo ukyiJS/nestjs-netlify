b9a2280b807b3cc3fb732ca9e54ef104eddd7bf4
ukyi <devcat.js@gmail.com>
2021-08-17 12:51:07 +0900

    feat: user entity 추가

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,46 @@
+import { ObjectType } from '@nestjs/graphql';
+import { Exclude, plainToClass } from 'class-transformer';
+import { Column, Entity, ObjectIdColumn } from 'typeorm';
+import { v4 } from 'uuid';
+import { CardType, GenderType } from '@/types/user';
+
+@ObjectType()
+class Card {
+  type: CardType;
+  number: string;
+}
+
+@Entity()
+@ObjectType()
+export class User {
+  @Exclude() @ObjectIdColumn({ type: 'uuid' })
+  _id: string;
+  @Column()
+  name: string;
+  @Column()
+  password: string;
+  @Column()
+  gender: GenderType;
+  @Column()
+  email: string;
+  @Column()
+  city: string;
+  @Column()
+  image: string;
+  @Column()
+  card: Card;
+  @Column()
+  languages: string[];
+  @Column()
+  rowNumber: number;
+  @Exclude() @Column()
+  createdAt: Date;
+
+  constructor(user: Partial<User>) {
+    if (user?.rowNumber) return;
+
+    Object.assign(this, plainToClass(User, user));
+    this._id = this._id ?? v4();
+    this.createdAt = this.createdAt ?? new Date();
+  }
+}
