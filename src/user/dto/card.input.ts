4fca559f62001715fe64221b578044e7e7be5714
ukyi <devcat.js@gmail.com>
2021-08-17 17:20:25 +0900

    update: IsCreditCard로 수정

--- src/user/dto/card.input.ts
+++ src/user/dto/card.input.ts
@@ -1,11 +1,11 @@
 import { InputType } from '@nestjs/graphql';
-import { IsEnum, IsNumberString } from 'class-validator';
+import { IsCreditCard, IsEnum } from 'class-validator';
 import { CardType } from '@/types/user';
 
 @InputType()
 export class CardInput {
   @IsEnum(CardType, { message: '잘못된 카드 타입입니다.' })
   type?: CardType;
-  @IsNumberString(undefined, { message: '잘못된 카드 번호 입니다.' })
+  @IsCreditCard({ message: '잘못된 카드 번호 입니다.' })
   number?: string;
 }
