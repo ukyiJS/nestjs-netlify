f85328b2f1cf84b3a94b2e007045b1d83d1ce2c3
ukyi <devcat.js@gmail.com>
2021-08-18 17:17:30 +0900

    update: OmitType user extneds로 수정

--- src/user/dto/user.args.ts
+++ src/user/dto/user.args.ts
@@ -1,20 +1,8 @@
-import { ArgsType } from '@nestjs/graphql';
-import { IsEmail, IsEnum, IsUrl, Length } from 'class-validator';
-import { GenderType } from '@/types/user';
-import { CardInput } from '@/user/dto/card.input';
+import { ArgsType, OmitType } from '@nestjs/graphql';
+import { User } from '../entities/user.entity';
+import { CardInput } from './card.input';
 
 @ArgsType()
-export class UserArgs {
-  name: string;
-  @Length(6, 12, { message: '비밀번호는 6글자 이상 12글자 이하여야 합니다.' })
-  password: string;
-  @IsEnum(GenderType, { message: '잘못된 성별입니다.' })
-  gender: GenderType;
-  @IsEmail(undefined, { message: '잘못된 이메일 형식입니다.' })
-  email: string;
-  city: string;
-  @IsUrl(undefined, { message: '잘못된 이미지 URL 형식입니다' })
-  image: string;
+export class UserArgs extends OmitType(User, ['_id', 'card'], ArgsType) {
   card: CardInput;
-  languages: string[];
 }
