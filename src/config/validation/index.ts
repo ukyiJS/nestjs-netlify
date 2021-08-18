35af2402a69c5581e5a03bf8554ddb0f8ea6d9cb
ukyi <devcat.js@gmail.com>
2021-08-18 18:54:58 +0900

    feat: validation children  추가 @ValidateNested() 데코레이터 사용하면 children 객체가 생성됨

--- src/config/validation/index.ts
+++ src/config/validation/index.ts
@@ -1,12 +1,17 @@
 import { BadRequestException, HttpStatus, ValidationPipeOptions } from '@nestjs/common';
 import { ValidationError } from 'class-validator';
+import { ObjectLiteral } from '@/types';
 
 export const ValidationOptions: ValidationPipeOptions = {
   transform: true,
   errorHttpStatusCode: HttpStatus.BAD_REQUEST,
   skipMissingProperties: true,
   exceptionFactory: (validationErrors: ValidationError[] = []): BadRequestException => {
-    const errors = validationErrors.map(e => ({ [e.property]: Object.values(e.constraints ?? {})[0] }));
+    const getValidation = ({ property, constraints = {} }:ValidationError): ObjectLiteral => ({ [property]: Object.values(constraints)[0] });
+    const errors = validationErrors.reduce<ObjectLiteral>((acc, error) => {
+      const { children = [] } = error;
+      return acc.concat(children.length ? children.map(getValidation) : getValidation(error));
+    }, []);
     return new BadRequestException(errors, 'VALIDATION_ERROR');
   },
 };
