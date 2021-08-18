7ffdd2b5e9502b0bc8c9755a418d9c877034752a
ukyi <devcat.js@gmail.com>
2021-08-18 20:07:27 +0900

    feat: FindCardInput 추가 및 ValidateNested 추가

--- src/user/dto/find-user.args.ts
+++ src/user/dto/find-user.args.ts
@@ -1,5 +1,15 @@
-import { ArgsType, PartialType } from '@nestjs/graphql';
+import { ArgsType, InputType, OmitType, PartialType } from '@nestjs/graphql';
+import { Type } from 'class-transformer';
+import { ValidateNested } from 'class-validator';
+import { CardInput } from '@/user/dto/card.input';
 import { UserArgs } from './user.args';
 
+@InputType()
+class FindCardInput extends PartialType(CardInput) {}
+
 @ArgsType()
-export class FindUserArgs extends PartialType(UserArgs) {}
+export class FindUserArgs extends OmitType(PartialType(UserArgs), ['card'] as const) {
+  @Type(() => FindCardInput)
+  @ValidateNested()
+  card?: FindCardInput;
+}
