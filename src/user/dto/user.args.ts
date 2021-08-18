01a68eeebe6892a26637a5dc6e7fd89cfa25b056
ukyi <devcat.js@gmail.com>
2021-08-18 20:08:58 +0900

    feat: ValidateNested 추가

--- src/user/dto/user.args.ts
+++ src/user/dto/user.args.ts
@@ -1,8 +1,12 @@
 import { ArgsType, OmitType } from '@nestjs/graphql';
+import { Type } from 'class-transformer';
+import { ValidateNested } from 'class-validator';
+import { CardInput } from '@/user/dto/card.input';
 import { User } from '../entities/user.entity';
-import { CardInput } from './card.input';
 
 @ArgsType()
-export class UserArgs extends OmitType(User, ['_id', 'card'], ArgsType) {
+export class UserArgs extends OmitType(User, ['_id', 'card', 'createdAt'] as const, ArgsType) {
+  @Type(() => CardInput)
+  @ValidateNested()
   card: CardInput;
 }
